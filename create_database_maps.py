1d96a62edebd5138c44fa3c9129f92c5e9c2fe27
Drew Leonard <andy.j.leonard@gmail.com>
2014-09-04 11:11:42 +0100

    Fixed stupid mistakes

--- create_database_maps.py
+++ create_database_maps.py
@@ -10,6 +10,7 @@ use('agg')
 import matplotlib.pyplot as plt
 from temperature import TemperatureMap
 import datetime as dt
+from os import system as sys
 
 years = range(2010, 2015)
 months = range(1, 13)
@@ -26,9 +27,15 @@ for year in years:
             thismap.plot(cmap='gist_heat', vmin=0.6, vmax=2.5)#vmin=5.8, vmax=6.6)
             plt.title('Coronal temperature in MK')
             plt.colorbar()
-            plt.savefig(date)
+            mapdir = '/media/huw/temperature_maps/1pars/maps/'
+            error = sys('touch {}{:%Y/%m/%d/}'.format(mapdir, date))
+            if error != 0:
+                sys('{0}{1:%Y}; {0}{1:%Y/%m}; {0}{1:%Y/%m/%d}'.format(
+                        'mkdir '+mapdir, date))
+            plt.savefig(mapdir+'{:%Y/%m/%d/%Y-%m-%dT%H:%M:%S}'.format(date))
             plt.close()
         except:
+            print 'Failed', date
             failed.append(date)
 
 print 'Failed to create temperature maps for the following dates:'
