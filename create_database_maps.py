057530a2f28d0e3a665bd00d5c061b1a64a2d8f8
Drew Leonard <andy.j.leonard@gmail.com>
2014-09-30 16:07:16 +0100

    Changes to create_database_maps, apparently

--- create_database_maps.py
+++ create_database_maps.py
@@ -16,6 +16,7 @@ from temperature import TemperatureMap
 
 years = range(2010, 2015)
 months = range(1, 13)
+days = [1, 15]
 failed = []
 dates = []
 means = []
@@ -25,30 +26,32 @@ stds = []
 
 for year in years:
     for month in months:
-        try:
-            date = dt.datetime(year, month, 1)
-            thismap = TemperatureMap(date)
-            thismap.save()
-            thismap.convert_scale('linear')
-            fig = plt.figure(figsize=(16,12))
-            thismap.plot(cmap='gist_heat', vmin=0.6, vmax=2.5)#vmin=5.8, vmax=6.6)
-            plt.title('Coronal temperature in MK')
-            plt.colorbar()
-            mapdir = '/media/huw/temperature_maps/1pars/maps/'
-            error = sys('touch {}{:%Y/%m/%d/}'.format(mapdir, date))
-            if error != 0:
-                sys('{0}{1:%Y}; {0}{1:%Y/%m}; {0}{1:%Y/%m/%d}'.format(
-                        'mkdir '+mapdir, date))
-            plt.savefig(mapdir+'{:%Y/%m/%d/%Y-%m-%dT%H:%M:%S}'.format(date))
-            plt.close()
-            dates.append(str(date))
-            means.append(np.nanmean(thismap.data, dtype='float64'))
-            mins.append(thismap.min())
-            maxes.appens(thismap.max())
-            stds.append(np.nanstd(thismap.data, dtype='float64'))
-        except:
-            print 'Failed', date
-            failed.append(date)
+        for day in days:
+            try:
+                date = dt.datetime(year, month, day)
+                thismap = TemperatureMap(date)
+                thismap.save()
+                thismap.convert_scale('linear')
+                fig = plt.figure(figsize=(16,12))
+                #thismap.plot(cmap='gist_heat', vmin=0.6, vmax=2.5)#vmin=5.8, vmax=6.6)
+                thismap.plot(cmap='RdYlBu_r', vmin=0.6, vmax=2.5)
+                plt.title('Coronal temperature in MK')
+                plt.colorbar()
+                mapdir = '/media/huw/temperature_maps/1pars/maps/'
+                error = sys('touch {}{:%Y/%m/%d/}'.format(mapdir, date))
+                if error != 0:
+                    sys('{0}{1:%Y}; {0}{1:%Y/%m}; {0}{1:%Y/%m/%d}'.format(
+                            'mkdir '+mapdir, date))
+                plt.savefig(mapdir+'{:%Y/%m/%d/%Y-%m-%dT%H:%M:%S}'.format(date))
+                plt.close()
+                dates.append(str(date))
+                means.append(np.nanmean(thismap.data, dtype='float64'))
+                mins.append(thismap.min())
+                maxes.append(thismap.max())
+                stds.append(np.nanstd(thismap.data, dtype='float64'))
+            except:
+                print 'Failed', date
+                failed.append(date)
 
 print 'Failed to create temperature maps for the following dates:'
 for f in failed:
