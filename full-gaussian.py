c5f371a757524cc54c3447e92d608fce11695cb6
Andrew Leonard <andy.j.leonard@gmail.com>
2015-04-27 18:45:30 +0100

    Comment out 1-param stuff
    Full Gaussian version is loading the 1-param saved tmap, so it has to go.
    I've got the plot out of it, so it's fine

--- full-gaussian.py
+++ full-gaussian.py
@@ -9,18 +9,18 @@ from temperature import TemperatureMap as tmap
 #data_dir = path.expanduser('~/CoronaTemps/')
 maps_dir = path.expanduser('~/CoronaTemps/')
 data_dir = '/imaps/sspfs/archive/sdo/aia/fulldisk/data/'
-thismap1 = tmap('2011-02-15', data_dir=data_dir, maps_dir=maps_dir)
+#thismap1 = tmap('2011-02-15', data_dir=data_dir, maps_dir=maps_dir)
 thismap3 = tmap('2011-02-15', data_dir=data_dir, maps_dir=maps_dir, n_params=3, verbose=True)
 
-thismap1.save()
+#thismap1.save()
 print thismap3.shape
 print np.nanmin(thismap3.data[..., 0]), np.nanmean(thismap3.data[..., 0]), np.nanmax(thismap3.data[..., 0])
 
-fig = plt.figure(figsize=(14, 14))
+"""fig = plt.figure(figsize=(14, 14))
 thismap1.plot()
 plt.colorbar()
 plt.savefig(path.expanduser('~/CoronaTemps/1paramtemps'))
-plt.close()
+plt.close()"""
 
 fig = plt.figure(figsize=(14, 14))
 thismap3.plot()
