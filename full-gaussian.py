c7d3f267c923a6893e2e8e4b3943b154eaf376ad
Andrew Leonard <andy.j.leonard@gmail.com>
2015-04-28 10:58:56 +0100

    Save both maps and tweak output

--- full-gaussian.py
+++ full-gaussian.py
@@ -10,12 +10,14 @@ data_dir = path.expanduser('~/CoronaTemps/')
 maps_dir = path.expanduser('~/CoronaTemps/')
 data_dir = '/imaps/sspfs/archive/sdo/aia/fulldisk/data/'
 thismap1 = tmap('2011-02-15', data_dir=data_dir, maps_dir=maps_dir, verbose=True)
-thismap3 = tmap('2011-02-15', data_dir=data_dir, maps_dir=maps_dir, n_params=3, verbose=True)
-
 thismap1.save()
+thismap3 = tmap('2011-02-15', data_dir=data_dir, maps_dir=maps_dir, n_params=3, verbose=True)
+thismap3.save()
+
 print thismap3.shape
+print thismap3.gaussian_parameters
 print np.nanmin(thismap1.data), np.nanmean(thismap1.data), np.nanmax(thismap1.data)
-print np.nanmin(thismap3.data[..., 0]), np.nanmean(thismap3.data[..., 0]), np.nanmax(thismap3.data[..., 0])
+print np.nanmin(thismap3.data), np.nanmean(thismap3.data), np.nanmax(thismap3.data)
 
 fig = plt.figure(figsize=(14, 14))
 thismap1.plot()
