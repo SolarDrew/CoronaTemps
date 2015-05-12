08fe152ed040de69ac0a97bcdb62adb2d113647f
Andrew Leonard <andy.j.leonard@gmail.com>
2015-05-12 14:48:23 +0100

    Move functions out of create_tempmap into new utils script

--- create_tempmap.py
+++ create_tempmap.py
@@ -23,6 +23,7 @@ from sunpy.time.timerange import TimeRange as tr
 import glob
 from itertools import product
 from mpi4py import MPI
+from utils import gaussian, load_temp_responses
 try:
     from fits import calc_fits
     print 'Fortran extension imported successfully'
@@ -37,36 +38,6 @@ home = path.expanduser('~')
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 size = comm.Get_size()
-
-
-def gaussian(x, mean=0.0, std=1.0, amp=1.0):
-    """Simple function to return a Gaussian distribution"""
-    if isinstance(x, list):
-        x = np.array(x)
-    power = -((x - mean) ** 2.0) / (2.0 * (std ** 2.0))
-    f = amp * np.exp(power)
-    if amp == 1:
-        f = f / max(f)
-    return f
-
-
-def load_temp_responses(n_wlens=6, corrections=True):
-    resp = np.zeros((n_wlens, 301))
-    tresp = read('{}/CoronaTemps/aia_tresp'.format(home))
-    resp[0, 80:181] = tresp['resp94']
-    resp[1, 80:181] = tresp['resp131']
-    resp[2, 80:181] = tresp['resp171']
-    resp[3, 80:181] = tresp['resp193']
-    resp[4, 80:181] = tresp['resp211']
-    resp[5, 80:181] = tresp['resp335']
-    if n_wlens > 6:
-        resp[6, 80:181] = tresp['resp304']
-    if corrections:
-        # Add empirical correction factor for 9.4nm response function below log(T)=6.3
-        # (see Aschwanden et al 2011)
-        resp[0:126, 0] = resp[0:126, 0]*6.7
-    
-    return resp
 
 args = []
 for a in argv[1:]:
