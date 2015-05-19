23ce03e2bfc915e5346f2a0dae6e32d9f601241d
Andrew Leonard <andy.j.leonard@gmail.com>
2015-05-19 13:01:29 +0100

    Add plots for output width and EM

--- test_vs_model_DEMs.py
+++ test_vs_model_DEMs.py
@@ -18,6 +18,7 @@ from temperature import TemperatureMap
 from utils import gaussian, load_temp_responses
 from os import path, makedirs
 import subprocess32 as subp
+from itertools import product
 
 # Define CoronaTemps home folder and output folder
 CThome = path.join(path.expanduser('~'), 'CoronaTemps')
@@ -25,29 +26,34 @@ outdir = path.join(CThome, 'validation')
 if not path.exists(outdir): makedirs(outdir)
 
 # Define parameter ranges
-temps = np.arange(4.6, 7.405, 0.005)
+#temps = np.arange(4.6, 7.405, 0.005)
+temps = np.arange(5.6, 7.005, 0.005)
 widths = np.arange(0.01, 0.605, 0.005) # Just copying Aschwanden's range here
-heights = np.array([25])#, 25, 35])
+heights = 10 ** np.arange(25, 32, 0.25)
 print heights
 n_temps = len(temps)
 n_widths = len(widths)
 n_heights = len(heights)
+parvals = np.array([i for i in product(temps, widths, heights)])
+print parvals.shape
 n_vals = n_temps * n_widths * n_heights
 print n_temps, n_widths, n_heights, n_vals, n_vals * 6
 
 # Create model DEMs and synthetic emission
-emission = np.zeros((6, n_temps, n_heights, n_widths))
+emission = np.zeros((6, n_temps, n_widths, n_heights))
+print emission.shape
 logt = np.arange(0, 15.05, 0.05)
 resp = load_temp_responses()
 delta_t = logt[1] - logt[0]
-for t, meantemp in enumerate(temps):
-    for h, height in enumerate(heights):
-        for w, width in enumerate(widths):
-            dem = gaussian(logt, meantemp, width, height)
-            f = resp * dem
-            emission[:, t, h, w] = np.sum(f, axis=1) * delta_t
+for p, params in enumerate(parvals):
+    dem = gaussian(logt, *params)
+    f = resp * dem
+    t = np.where(temps == params[0])[0][0]
+    w = np.where(widths == params[1])[0][0]
+    h = np.where(heights == params[2])[0][0]
+    emission[:, t, w, h] = np.sum(f, axis=1) * delta_t
 
-emission = emission / emission[2, :, :, :]
+#emission = emission / emission[2, :, :, :]
 
 # Load AIA response functions
 resp = load_temp_responses()
@@ -57,73 +63,130 @@ voidmap = Map(sunpy.AIA_171_IMAGE)
 mapmeta = voidmap.meta
 
 # Run synthetic data through 1param tempmap method
-for w, wid in enumerate(heights):#widths):
+for w, wid in enumerate(widths):#heights):
     print 'Width:', wid
-    fig = plt.figure(figsize=(30, 12))
+    #fig = plt.figure(figsize=(30, 12))
     for wl, wlength  in enumerate(['94', '131', '171', '193', '211', '335']):
+        #emiss = Map(emission[wl, :, :, w], mapmeta)
         emiss = Map(emission[wl, :, w, :], mapmeta)
         emiss.cmap = sunpy.cm.get_cmap('sdoaia{}'.format(wlength))
         emiss.meta['naxis1'] = emiss.shape[1]
         emiss.meta['naxis2'] = emiss.shape[0]
-        emiss.meta['cdelt1'] = widths[1] - widths[0]
+        #emiss.meta['cdelt1'] = widths[1] - widths[0]
+        emiss.meta['cdelt1'] = np.log10(heights[1]) - np.log10(heights[0])
         emiss.meta['cdelt2'] = temps[1] - temps[0]
-        emiss.meta['crval1'] = widths[0]
+        #emiss.meta['crval1'] = widths[0]
+        emiss.meta['crval1'] = np.log10(heights[0])
         emiss.meta['crval2'] = temps[0]
         emiss.meta['crpix1'] = 0.5
         emiss.meta['crpix2'] = 0.5
+        if wlength == '94': wlength = '094'
         fits_dir = path.join(CThome, 'data', 'synthetic', wlength)
         if not path.exists(fits_dir): makedirs(fits_dir)
-        emiss.save(path.join(fits_dir, 'model.fits'))
-        fig.add_subplot(1, 6, wl+1)
-        emiss.plot()#vmin=0.0, vmax=1.0)
-        plt.title('{}/171'.format(wlength))
+        emiss.save(path.join(fits_dir, 'model.fits'), clobber=True)
+        """fig.add_subplot(1, 6, wl+1)
+        emiss.plot(aspect='auto')#vmin=0.0, vmax=1.0)
+        plt.title('{}'.format(wlength))
         plt.xlabel('Input width')
         plt.ylabel('Input log(T)')
-        plt.colorbar()
-    plt.savefig(path.join(outdir, 'model_emission_h={}'.format(wid)))
+        plt.colorbar()"""
+    #plt.savefig(path.join(outdir, 'model_emission_h={}'.format(np.log10(wid)).replace('.', '_')))
+    #plt.savefig(path.join(outdir, 'model_emission_w={}'.format(wid).replace('.', '_')))
     plt.close()
     
+    #images = [Map(emission[i, :, :, w], mapmeta) for i in range(6)]
     images = [Map(emission[i, :, w, :], mapmeta) for i in range(6)]
     cmdargs = "mpiexec -n 1 python {} model {} {} {} {} {} {}".format(
-        path.join(CThome, 'create_tempmap.py'), 1, path.join(CThome, 'data'),
+        path.join(CThome, 'create_tempmap.py'), 3, path.join(CThome, 'data'),
         None, None, True, False).split()
     status = subp.call(cmdargs)
     newmap = Map(path.join(CThome, 'temporary.fits'))
     subp.call(["rm", path.join(CThome, 'temporary.fits')])
-    data, meta = newmap.data, newmap.meta
+    data, meta = newmap.data[..., 0], newmap.meta
+    wdata = newmap.data[..., 1]
+    emdata = newmap.data[..., 2]
+    newmap.data = data
+    #print '\n-----------MINMAX: ', newmap.min(), newmap.max(), newmap.shape, 
+    #print newmap.data[newmap.data == 0].shape, '----------\n'
 
-    truetemp = np.array(list(temps)*n_widths).reshape((n_widths, n_temps)).T
-    diff = (abs(truetemp - data) / truetemp) * 100
+    #truetemp = np.array(list(temps)*n_widths).reshape((n_widths, n_temps)).T
+    truetemp = np.array(list(temps)*n_heights).reshape((n_heights, n_temps)).T
+    truew = np.ones(shape=(n_temps, n_heights)) * wid
+    #print 'truew', truew.min(), truew.mean(), truew.max()
+    trueem = np.log10(np.array(list(heights)*n_temps).reshape(n_temps, n_heights))
+    diff = Map((abs(truetemp - data) / truetemp) * 100, newmap.meta.copy())
+    diffw = Map((abs(truew - wdata) / truew) * 100, newmap.meta.copy())
+    #print 'wdata', wdata.min(), wdata.mean(), wdata.max()
+    #print 'diffw', diffw.min(), diffw.mean(), diffw.max()
+    diffem = Map((abs(trueem - emdata) / trueem) * 100, newmap.meta.copy())
     
     fig = plt.figure(figsize=(24, 12))
     fig.add_subplot(1, 3, 1)
-    plt.imshow(data, cmap='coolwarm', origin='lower', aspect='auto',
-               extent=[widths[0], widths[-1], temps[0], temps[-1]],
-               interpolation='none', vmin=5.6, vmax=7.0)
+    newmap.plot(cmap='coolwarm', aspect='auto', vmin=5.6, vmax=7.0)
     plt.colorbar()
     plt.title('Solution log(T)', fontsize=28)
     plt.ylabel('Input log(T)', fontsize=24)
-    plt.xlabel('Input width', fontsize=24)
+    plt.xlabel('Input EM', fontsize=24)#width', fontsize=24)
     
     fig.add_subplot(1, 3, 2)
-    plt.imshow(diff, cmap='RdYlGn_r', origin='lower', aspect='auto',
+    """plt.imshow(diff, cmap='RdYlGn_r', origin='lower', aspect='auto',
                extent=[widths[0], widths[-1], temps[0], temps[-1]],
-               interpolation='none')
+               interpolation='none')"""
+    diff.plot(cmap='RdYlGn_r', aspect='auto')
     plt.colorbar()
     plt.title('Difference from input (%)', fontsize=28)
-    plt.xlabel('Input width', fontsize=24)
+    plt.xlabel('Input EM', fontsize=24)
     
-    fig.add_subplot(1, 3, 3)
+    """fig.add_subplot(1, 3, 3)
     plt.imshow(np.log10(fits), cmap='cubehelix', origin='lower', aspect='auto',
                extent=[widths[0], widths[-1], temps[0], temps[-1]],
                interpolation='none')#, vmin=fits.mean()-(2*fits.std()),
     plt.colorbar()
     plt.title('log(Goodness-of-fit)', fontsize=28)
-    plt.xlabel('Input width', fontsize=24)
-    plt.savefig('plots2/tempsolutions_em={:.2f}.pdf'.format(wid))
+    plt.xlabel('Input width', fontsize=24)"""
+    #plt.savefig(path.join(outdir, 'tempsolutions_em={}'.format(np.log10(wid)).replace('.', '_')))
+    plt.savefig(path.join(outdir, 'tempsolutions_wid={:.3f}'.format(wid).replace('.', '_')))
     plt.close()
+
+    fig = plt.figure(figsize=(24, 12))
+    fig.add_subplot(1, 3, 1)
+    Map(wdata, newmap.meta.copy()).plot(cmap='coolwarm', aspect='auto',
+                                        vmin=widths[0], vmax=widths[-1])
+    plt.colorbar()
+    plt.title('Solution width', fontsize=28)
+    plt.ylabel('Input log(T)', fontsize=24)
+    plt.xlabel('Input EM', fontsize=24)#width', fontsize=24)
     
-w = np.where((widths > 0.097)*(widths < 0.103))
+    fig.add_subplot(1, 3, 2)
+    diffw.plot(cmap='RdYlGn_r', aspect='auto', vmin=diffw.min(), vmax=diffw.max())
+    plt.colorbar()
+    plt.title('Difference from input (%)', fontsize=28)
+    plt.xlabel('Input EM', fontsize=24)
+    
+    plt.savefig(path.join(outdir, 'widsolutions_wid={:.3f}'.format(wid).replace('.', '_')))
+    plt.close()
+
+    fig = plt.figure(figsize=(24, 12))
+    fig.add_subplot(1, 3, 1)
+    Map(emdata, newmap.meta.copy()).plot(cmap='coolwarm', aspect='auto',
+                                         vmin=np.log10(heights[0]),
+                                         vmax=np.log10(heights[-1]))
+    plt.colorbar()
+    plt.title('Solution EM', fontsize=28)
+    plt.ylabel('Input log(T)', fontsize=24)
+    plt.xlabel('Input EM', fontsize=24)#width', fontsize=24)
+    
+    fig.add_subplot(1, 3, 2)
+    diffem.plot(cmap='RdYlGn_r', aspect='auto', vmin=diffem.min(),
+                vmax=diffem.max())
+    plt.colorbar()
+    plt.title('Difference from input (%)', fontsize=28)
+    plt.xlabel('Input EM', fontsize=24)
+    
+    plt.savefig(path.join(outdir, 'emsolutions_wid={:.3f}'.format(wid).replace('.', '_')))
+    plt.close()
+
+"""w = np.where((widths > 0.097)*(widths < 0.103))
 dataslice = data[:, w].reshape(len(temps))
 diffslice = diff[:, w].reshape(len(temps))
 fitslice = fits[:, w].reshape(len(temps))
@@ -151,7 +214,7 @@ plt.title('Goodness-of-fit at width=0.1', fontsize=28)
 plt.xlabel('Input log(T)', fontsize=24)
 plt.ylabel('log(Goodness-of-fit)', fontsize=24)
 plt.savefig('/home/drew/Dropbox/euroscipy/fitslice')
-plt.close()
+plt.close()"""
 
 # Run synthetic data throguh 3param tempmap method
 
