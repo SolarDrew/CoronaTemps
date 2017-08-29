210642175e063b39dd1878c2731f15bf34e4225f
Drew Leonard <andy.j.leonard@gmail.com>
2017-08-29 15:11:27 +0100

    New fortran file. No idea why it's there but I should probably keep it

---
+++ fitsmod_fullfit.f90
@@ -0,0 +1,41 @@
+SUBROUTINE calc_fits(images, model, parvals, n_vals, n_wlens, x, y, n_pars, results, all_fit)
+
+IMPLICIT NONE
+INTEGER, INTENT(IN) :: n_vals, n_wlens, x, y, n_pars
+REAL, DIMENSION(n_vals, n_pars), INTENT(IN) :: parvals
+REAL, DIMENSION(n_vals, n_wlens), INTENT(IN) :: model
+REAL, DIMENSION(n_wlens, x, y), INTENT(IN) :: images
+REAL, DIMENSION(x, y, n_pars+1), INTENT(OUT) :: results
+REAL, DIMENSION(x, y, n_vals), INTENT(OUT) :: all_fit
+!REAL, DIMENSION(n_vals), INTENT(OUT) :: all_fit
+INTEGER :: i, j, t, w
+REAL :: error, total_error, this_fit, best_fit
+
+!PRINT*, 'wtfishappening'
+DO j = 1, y
+  DO i = 1, x
+    best_fit = 1e38 ! Arbitrarily large number
+    DO t = 1, n_vals
+      total_error = 0.0
+      DO w = 1, n_wlens
+        error = ABS(images(w,i,j) - model(t, w))
+        total_error = total_error + error
+      END DO
+      this_fit = total_error / REAL(n_wlens)
+      all_fit(i, j, t) = this_fit
+      !IF (j == 14 .AND. i == 56) THEN
+      !!IF (j == 14 .AND. i == 186) THEN
+      !!IF (j == 14 .AND. i == 251) THEN
+      !  !PRINT*, i, j, t, this_fit
+      !  all_fit(t) = this_fit
+      !END IF
+      IF (this_fit < best_fit) THEN
+        best_fit = this_fit
+        results(i, j, 1:n_pars) = parvals(t, :)
+        results(i, j, n_pars+1) = this_fit
+      END IF
+    END DO
+  END DO
+END DO
+
+END SUBROUTINE calc_fits
